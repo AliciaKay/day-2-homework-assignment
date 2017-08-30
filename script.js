5459a11ff299b7a8569a54f3516173c6f7ed65aa
Alicia Wilking <aliciakaywilking@gmail.com>
2017-08-29 20:49:58 -0500

    incomplete edabit code challenge files with comments

---
+++ script.js
@@ -0,0 +1,26 @@
+//test that files are properly connected
+console.log('sourced')
+
+const MOVES = 
+["Shimmy", "Shake", "Pirouette", "Slide", "Box Step", 
+ "Headspin", "Dosado", "Pop", "Lock", "Arabesque"];
+function danceConvert(pin){
+    //take each digit and split them into separate items 
+    //in an array called pinArray
+    var pinArray = ("" + pin).split("");
+
+    //test the array function 
+    console.log('pinArray ->', pinArray)
+//convert each pinArray item into an integer
+    for (var i = 0; i < pinArray.length; i++);
+    parseInt(pinArray[i]);
+//store each integer into a new array called arrayInt
+    var arrayInt = [];
+    arrayInt.push[i];
+    console.log ('arrayInt ->', arrayInt);
+//add the integer to its index number from pinArray, 
+//store this sum in a variable called pinInt
+
+//
+
+}
