cba034119e2d6e67626836afa851b5f21dcacd17
Alicia Wilking <aliciakaywilking@gmail.com>
2017-08-29 20:53:20 -0500

    comments were not inititally saved with initial push

--- script.js
+++ script.js
@@ -19,8 +19,20 @@ function danceConvert(pin){
     arrayInt.push[i];
     console.log ('arrayInt ->', arrayInt);
 //add the integer to its index number from pinArray, 
-//store this sum in a variable called pinInt
+//store this sum in a variable called pinSum
 
-//
+//create an empty array called newDancePin
+
+//if the pin contains NaN, return "Invalid Input"
+
+//else if the variable pinSum is larger than the last index number 
+//of the MOVES array, push the item at index 0 to the newDancePin array
+// then move MOVES[0] to the end of the MOVES array
+
+//else, match the value of the variable pinSum to the corresponding 
+//index number of the MOVES array and push that value to the end of 
+//the newDancePin array
+
+//return newDancePin
 
 }
